55790f0c826430a41d47c02d428fcd46f7f82d04
Mehran028 <19011519-028@uog.edu.pk>
2023-11-07 19:20:13 +0500

    Portfolio

---
+++ Portfolio Project/main.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".text", {
+  strings: [
+    "WordPress Developer",
+    "Frontend Developer",
+    "Social Media Manager",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 500,
+  loop: true,
+});
